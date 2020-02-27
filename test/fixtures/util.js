4804d6c8dcafb2ef07fde5a8791f263337a38f69
Anton Gilgur <agilgur5@gmail.com>
2020-02-27 03:23:42 -0500

    attempt to use node-ramdisk and fail

--- test/fixtures/util.js
+++ test/fixtures/util.js
@@ -1,12 +1,21 @@
+const util = require('util');
+const ramdisk = require('node-ramdisk');
 const shell = require('shelljs');
 const path = require('path');
+
 const rootDir = process.cwd();
+const disk = ramdisk('tsdx-tests');
+const vol = (async () => {
+  await util.promisify(disk.delete)('/tmp/tsdx-tests');
+  return await util.promisify(disk.create)(10);
+})();
+console.log(vol);
 
 shell.config.silent = true;
 
 module.exports = {
   setupStageWithFixture: (stageName, fixtureName) => {
-    const stagePath = path.join(rootDir, stageName);
+    const stagePath = path.join(vol, stageName);
     shell.mkdir(stagePath);
     shell.exec(`cp -a ${rootDir}/test/fixtures/${fixtureName}/. ${stagePath}/`);
     shell.ln(
@@ -19,7 +28,7 @@ module.exports = {
 
   teardownStage: stageName => {
     shell.cd(rootDir);
-    shell.rm('-rf', path.join(rootDir, stageName));
+    shell.rm('-rf', path.join(vol, stageName));
   },
 
   rootDir,
